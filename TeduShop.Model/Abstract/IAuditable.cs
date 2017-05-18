22a5281940f04392bddcd15464cef9159cef9389
DESKTOP-UBC0EDN\DaoNguyen <dao.nbm1502@gmail.com>
2017-05-18 23:42:18 +0700

    Commit 2: Add model entities for project

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeduShop.Model.Models
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }//?: nullable
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }//?: nullable
+        string UpdatedBy { set; get; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+        bool Status { set; get; }
+
+    }
+}
