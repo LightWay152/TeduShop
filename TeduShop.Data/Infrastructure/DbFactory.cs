1a0504f9c79e677ac3a8972c26506b03b5874d6a
DESKTOP-UBC0EDN\DaoNguyen <dao.nbm1502@gmail.com>
2017-05-19 15:41:01 +0700

    Commit 3: Add Entity Framework Code First for Data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbConText;
+
+        public TeduShopDbContext Init()
+        {
+            return dbConText ?? (dbConText = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbConText != null)
+                dbConText.Dispose();
+        }
+    }
+}
